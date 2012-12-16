01448b9df922e3f6c2ee8110f87f67ad2a98878d
Gabor Barna <mr.barna.gabor@gmail.com>
2012-12-16 05:57:36 +0100

    Sun Dec 16 05:57:36 CET 2012

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,4 +1,4 @@
-(xbindkey '("XF86Sleep") "(xlock -mode blank &) && sudo systemctl suspend")
+(xbindkey '("XF86Sleep") "(i3lock -c 002a35 -u &) && sudo systemctl suspend")
 (xbindkey '("XF86AudioRaiseVolume") "amixer set Master 2dB+ unmute")
 (xbindkey '("XF86AudioLowerVolume") "amixer set Master 2dB- unmute")
 (xbindkey '("XF86AudioMute") "amixer set Master 0 mute")
